154af5c0c8f70afbe406a8ef3236498d9a99df8c
andronnem <andronnem@ukr.net>
2026-07-11 22:00:15 +0300

    Create sampleTasks.js

---
+++ sampleTasks.js
@@ -0,0 +1,27 @@
+import Task from "./task.js";
+
+export default [
+
+    new Task(
+        "Buy groceries"
+    ),
+
+    new Task(
+        "Finish portfolio",
+        true
+    ),
+
+    new Task(
+        "Read JavaScript book"
+    ),
+
+    new Task(
+        "Backup laptop",
+        true
+    ),
+
+    new Task(
+        "Walk the dog"
+    )
+
+];
